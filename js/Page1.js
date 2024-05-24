4a95a1b0a8a367ba41584d86bab011f43b0a5621
asdrt001 <t_suparat.jaroenpitak@scb.co.th>
2024-05-25 03:17:44 +0700

    Fea : JQuery

--- js/Page1.js
+++ js/Page1.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const http = require('http');
+const jquery = require('jquery')
 
 app.get('/Home', function (req, res) {
     
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
+
+    set.forEach(value => {
+        console.log(`Value: ${value}`);
+      });
 })
 
 app.listen(3000)
