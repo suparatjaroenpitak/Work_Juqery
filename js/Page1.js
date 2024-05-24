99c1617a8d5fc1746c84734714725c12eb42e222
asdrt001 <t_suparat.jaroenpitak@scb.co.th>
2024-05-25 06:54:19 +0700

    Feat Docker Ecommere

--- js/Page1.js
+++ js/Page1.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http');
 const jquery = require('jquery')
+const cheerio = require('cheerio');
 
 app.get('/Home', function (req, res) {
     
@@ -12,5 +13,26 @@ app.get('/Home', function (req, res) {
         console.log(`Value: ${value}`);
       });
 })
+
+
+// ตัวอย่าง HTML
+const html = `
+  <ul id="fruits">
+    <li class="apple">Apple</li>
+    <li class="orange">Orange</li>
+    <li class="pear">Pear</li>
+  </ul>
+`;
+
+// โหลด HTML ด้วย Cheerio
+const $ = cheerio.load(html);
+
+// ใช้ API คล้ายกับ jQuery
+const fruits = [];
+$('#fruits').find('li').each((index, element) => {
+  fruits.push($(element).text());
+});
+
+console.log(fruits); // ['Apple', 'Orange', 'Pear']
 
 app.listen(3000)
